ac12c8ed05804fe1d9ae2c0f0032ba1a388bf964
Julio Cesar <juliocesargcosta123@gmail.com>
2024-09-23 00:56:44 +0100

    feat/adding the requirements - added the requirements for both development environment and for the robot environment - added a bash script to automatically configure the virtual environment for the llm api

--- requirements-robot.sh
+++ requirements-robot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the virtual environment
-python -m venv venv
+python3 -m venv venv
 # Activate the venv environment
 .\venv\Scripts\activate
 # Install the dependencies to the robot environment
